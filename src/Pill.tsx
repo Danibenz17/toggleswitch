fe5db790cbf95ee759cc302365dbc2180ee84250
Danibenz17 <danibenzhdb117@gmail.com>
2023-11-08 10:18:39 +0530

    Pill Created

--- src/Pill.tsx
+++ src/Pill.tsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import ToggleSwitch from "./ToggleSwitch";
 
 const PillToggleSwitch = () => {
-  const [isGreen, setIsGreen] = useState(true);
+  const [isOf, setIsOf] = useState(true);
   const handleToggle = (isChecked: boolean) => {
-    setIsGreen(isChecked);
+    setIsOf(isChecked);
   };
 
   return (
-    <div className={`pill ${isGreen ? "red" : "green"}`}>
+    <div className={`pill ${isOf ? "red" : "green"}`}>
       <ToggleSwitch
         leftLabel="Include"
         rightLabel="Exclude"
         onToggle={handleToggle}
-        initialChecked={isGreen}
+        initialChecked={isOf}
       />
     </div>
   );
