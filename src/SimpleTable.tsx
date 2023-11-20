625aee34f80cdfb61144e51d95932ce5a341277e
Danibenz17 <danibenzhdb117@gmail.com>
2023-11-20 11:11:09 +0530

    Changes

--- src/SimpleTable.tsx
+++ src/SimpleTable.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useContext, useState } from "react";
+import React, { useReducer, useState } from "react";
 
 interface RowData {
   id: number;
@@ -97,7 +97,6 @@ const reducer = (state: State, action: Action): State => {
       return state;
   }
 };
-
 
 const DataTable = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
